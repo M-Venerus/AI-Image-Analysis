8bcfc27f02f617f9b4cf9be4388fdc03c9c48306
M.Venerus Desktop Windows <mikey.motorist@outlook.com>
2024-03-20 22:37:53 -0400

    Add azure key vault config to Program.cs file. Create ApiService.cs file, inject Iconfig to use public strings for API Key and Endpoint URL. Register ApiService in Program.cs. Inject ApiService into Home Page component and define key and endpoint variables to take API key and Endpoint URL.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
 using AI_Image_Analysis.Components;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Identity;
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+// Add ApiService
+builder.Services.AddSingleton<ApiService>();
+
+// Configure Azure Key Vault
+var secretClient = new SecretClient(new Uri(builder.Configuration["VaultUri"]), new DefaultAzureCredential());
+builder.Configuration.AddAzureKeyVault(secretClient, new KeyVaultSecretManager());
+
 
 var app = builder.Build();
 
