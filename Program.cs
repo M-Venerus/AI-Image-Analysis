389c5b1ad3a1535e2e8220027209ac5aec4a90a3
M.Venerus Desktop Windows <mikey.motorist@outlook.com>
2024-03-21 20:38:25 -0400

    Style UI to look clean, neat, and attractive. Remove unnecessary pages and other blazor stock template components.

--- Program.cs
+++ Program.cs
@@ -19,19 +19,9 @@ builder.Configuration.AddAzureKeyVault(secretClient, new KeyVaultSecretManager()
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
-
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
